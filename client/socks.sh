e23b3657a440152ecd7edbc00a52905c54f10ebe
figroc <figroc@gmail.com>
2018-06-01 00:43:31 +0800

    keep alive

--- client/socks.sh
+++ client/socks.sh
@@ -5,5 +5,10 @@ port=${2}
 
 socks="localhost:9050"
 
-ssh -fNT -i~/.ssh/id_rsa -L9050:${socks} -oExitOnForwardFailure=yes -p${port} gof@${host} \
+ssh -fNT -i~/.ssh/id_rsa \
+    -L9050:${socks} \
+    -oExitOnForwardFailure=yes \
+    -oServerAliveInterval=30 \
+    -oServerAliveCountMax=5 \
+    -p${port} gof@${host} \
   && echo && echo "SOCKS5 Server: ${socks}"
