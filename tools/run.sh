37c9284a7d7177c31c5766cd5fb2a74e1aa50a2d
figroc <figroc@gmail.com>
2018-05-31 22:59:54 +0800

    fix permission

--- tools/run.sh
+++ tools/run.sh
@@ -4,13 +4,12 @@ name=${1}
 port=${2}
 torc=${3}
 
-function realpath {
-  echo $(cd $(dirname ${1}) && pwd)
-}
+pub=$(cd $(dirname ${0}) && pwd)/../users/${name}.pub
+chmod 644 ${pub}
 
 docker run --rm \
   --name ${name}_${port} \
   -p ${port}:22 \
   -e TOR_CC="${torc}" \
-  -v $(realpath ${0})/../users/${name}.pub:/etc/ssh/authorized_keys:ro \
+  -v ${pub}:/etc/ssh/authorized_keys:ro \
   -d figroc/psocks
