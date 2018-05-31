5a92dc2f0251569a9a5d57572f6ef4b1dc1eda10
figroc <figroc@gmail.com>
2018-05-31 23:08:31 +0800

    more friendly

--- client/socks.sh
+++ client/socks.sh
@@ -3,4 +3,7 @@
 host=${1}
 port=${2}
 
-ssh -fNT -i~/.ssh/id_rsa -L9050:localhost:9050 -p${port} gof@${host}
+socks="localhost:9050"
+
+ssh -fNT -i~/.ssh/id_rsa -L9050:${socks} -p${port} gof@${host} \
+  && echo && echo "SOCKS5 Server: ${socks}"
