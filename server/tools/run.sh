100143c78572b43fbfd9cdb0059841a8897311d4
figroc <figroc@gmail.com>
2018-05-31 16:34:00 +0800

    abs path

--- server/tools/run.sh
+++ server/tools/run.sh
@@ -4,9 +4,13 @@ name=${1}
 port=${2}
 torc=${3}
 
+function realpath {
+  echo $(cd $(dirname ${1}) && pwd)
+}
+
 docker run --rm \
-    --name ${name}_${port} \
-    -p ${port}:22 \
-    -e TOR_CC="${torc}" \
-    -v $(dirname ${0})/../users/${name}.pub:/etc/ssh/authorized_keys:ro \
-    -d figroc/psocks
+  --name ${name}_${port} \
+  -p ${port}:22 \
+  -e TOR_CC="${torc}" \
+  -v $(realpath ${0})/../users/${name}.pub:/etc/ssh/authorized_keys:ro \
+  -d figroc/psocks
