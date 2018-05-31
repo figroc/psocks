6625d149008bff9f55cf1478ec4e0fbff67f6841
figroc <figroc@gmail.com>
2018-05-31 23:30:47 +0800

    exit on failure

--- client/socks.sh
+++ client/socks.sh
@@ -5,5 +5,5 @@ port=${2}
 
 socks="localhost:9050"
 
-ssh -fNT -i~/.ssh/id_rsa -L9050:${socks} -p${port} gof@${host} \
+ssh -fNT -i~/.ssh/id_rsa -L9050:${socks} -oExitOnForwardFailure=yes -p${port} gof@${host} \
   && echo && echo "SOCKS5 Server: ${socks}"
