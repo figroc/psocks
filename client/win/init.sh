1291bf04d8473cf6fe42f3b7aa7b72e236790cbd
figroc <figroc@gmail.com>
2018-05-31 22:22:36 +0800

    new user

--- client/win/init.sh
+++ client/win/init.sh
@@ -3,4 +3,9 @@
 if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 2048
 fi
+echo
+echo
+echo "*******************************"
+echo "send following to administrator"
+echo "*******************************"
 cat ~/.ssh/id_rsa.pub
