5100186d3fd51d40b8007eda0aff5c5a814a6a3b
figroc <figroc@gmail.com>
2018-06-03 11:44:09 +0800

    more flexible

--- client/socks.sh
+++ client/socks.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 
-if (( ${#} != 2 )); then
-  echo "${0} <host> <port>" 1>&2
+if (( ${#} != 1 )); then
+  echo "${0} <host:port> [local-port]" 1>&2
   exit 1
 fi
 
-host=${1}
-port=${2}
+rhost=${1%%:*}
+rport=${1##*:}
+if [[ "${rhost}" == "${rport}" ]]; then
+  echo "<host:port> invalid: ${1}" 1>&2
+  exit 1
+fi
+
+lhost=${2%%:*}
+lport=${2##*:}
+if [[ "${lhost}" == "${lport}" ]]; then
+  lhost="localhost"
+fi
+if [[ -z "${lport}" ]]; then
+  lport="9050"
+fi
+
+rsocks="localhost:9050"
+lsocks="${lhost}:${lport}"
 
 waste=~/waste.ip
-socks="localhost:9050"
 echoi="http://ipecho.net/plain"
 
 set -e
 
 while :; do
-  echo renew...
   ssh -i~/.ssh/id_rsa \
-      -p${port} gof@${host} \
-      "/renew"
-  sleep 7
+      -p${rport} gof@${rhost} \
+      "/renew" | \
+      grep -Fv "250 closing connection"
+  echo -n waiting... && sleep 7 && echo
   ipn=$(ssh -i~/.ssh/id_rsa \
-            -p${port} gof@${host} \
-            "curl -sS --socks5 ${socks} ${echoi}")
+            -p${rport} gof@${rhost} \
+            "curl -sS --socks5 ${rsocks} ${echoi}")
   if ! grep ${ipn} ${waste} &>/dev/null; then
     break
   fi
 done
 
 echo
-echo ${ipn} | tee -a ${waste}
+echo -n "ExitIP: " && echo ${ipn} | tee -a ${waste}
 echo
 
 ssh -i~/.ssh/id_rsa \
-    -L9050:${socks} \
+    -L${lsocks}:${rsocks} \
     -oExitOnForwardFailure=yes \
     -oServerAliveInterval=30 \
     -oServerAliveCountMax=5 \
-    -p${port} gof@${host} \
-    "echo socks5://${socks} && sleep 365d"
+    -p${rport} gof@${rhost} \
+    "echo socks5://${lsocks} && echo && echo 'press <Ctrl-C> to quit' && sleep 365d"
